bde0f465d2d2ca20b5f70b3d2657dc5f597959fa
Alexandre Salaun <asalaun@siliconsalad.com>
2014-08-20 18:37:27 +0200

    Add whenever recipe

---
+++ whenever/CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG for whenever
+
+## 0.1.0:
+
+* Initial release of whenever
---
+++ whenever/metadata.rb
@@ -0,0 +1,7 @@
+name             'whenever'
+maintainer       'Alexandre Salaun'
+maintainer_email ''
+license          'All rights reserved'
+description      'Configures whenever'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
